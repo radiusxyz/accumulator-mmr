476b2a78aec237ae7376ded188bf2d1ef21c69d9
hyemmie <lhm0710@naver.com>
2022-02-20 03:06:33 +0900

    edit structure module to hash module

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,14 @@ mod mmr_store;
 #[cfg(test)]
 mod tests;
 pub mod util;
-mod structure;
+mod hash;
 
 pub use error::{Error, Result};
 pub use helper::{leaf_index_to_mmr_size, leaf_index_to_pos};
 pub use merge::Merge;
 pub use mmr::{MerkleProof, MMR};
 pub use mmr_store::MMRStore;
-pub use structure::{StringHash, MergeStringHash};
+pub use hash::{StringHash, MergeStringHash};
 
 cfg_if::cfg_if! {
     if #[cfg(feature = "std")] {
